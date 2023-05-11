1e32c8aa0bd0e8d7c497856e20d8bb0b91f3ee5a
firdoushsiddique62 <firdoushsiddique62@gmail.com>
2023-05-11 16:28:26 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        RWOnly obj=new RWOnly();
+       // RWOnly obj1=new RWOnly();
+
+
+
+        obj.setName("Firdoush");
+        //obj1.setName("Rohan");
+
+        String name=obj.getName();
+        //String name1=obj1.getName();
+
+        //java: cannot find symbol
+
+        System.out.println(name);
+       // System.out.println(name1);
+
+
+
+    }
   
 }
